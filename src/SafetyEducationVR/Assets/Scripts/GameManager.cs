14ccbfcdc8ee00aab1d956ccb0298e3d3d6d0ac0
donghyun94 <dhk9464@gmail.com>
2020-04-17 21:07:50 +0900

    화재안전 act1 초반

---
+++ src/SafetyEducationVR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject vrCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
